0cf10e4dc9ffeb1e327f65033dd796e4461c4ae3
jbernett <jbernettstl@gmail.com>
2020-12-31 17:51:41 -0600

    first commit

---
+++ DiceRoller/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DiceRoller.Models;
+
+
+namespace DiceRoller.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            DiceVM d = new DiceVM();
+            return View(d);
+        }
+
+        [HttpPost]
+        public ViewResult Index(DiceVM dice)
+        {
+            dice.GetTotal(dice.selectedDie, dice.amount);
+            return View(dice);
+        }
+    }
+}
---
+++ DiceRoller/Models/DiceVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiceRoller.Models
+{
+    public class DiceVM
+    {
+        public int selectedDie { get; set; }
+        public int amount { get; set; }
+        public int total { get; set; }
+
+        public int GetTotal(int selectedDie, int amount)
+        {
+            Random generator = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                total += generator.Next(selectedDie)+1;
+            }
+            return total;
+        }
+    }
+}
